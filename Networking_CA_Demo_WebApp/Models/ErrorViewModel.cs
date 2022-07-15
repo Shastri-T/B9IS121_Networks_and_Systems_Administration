de0c4e6286d335de080a7cad0586254c903b11cc
shas556 <105118561+shas556@users.noreply.github.com>
2022-07-15 11:16:46 +0100

    pushing project files and report

---
+++ Networking_CA_Demo_WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Networking_CA_Demo_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
